c2b76b6c46208baa4bfdfc7051eb32b4dfa60450
cntn-jr <cinochinotin@gmail.com>
2020-09-07 13:58:33 +0900

    initial commit

---
+++ app/src/main/java/com/kbc19a21/example/mycalculator/MainActivity.java
@@ -0,0 +1,14 @@
+package com.kbc19a21.example.mycalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
